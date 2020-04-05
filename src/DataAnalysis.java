4fdae9d232bb53fed66bc93d7807c7e2b86ff83d
calebbusch <cjbusch@seas.upenn.edu>
2020-04-05 10:56:14 -0700

    Added hashmap-creating methods

--- src/DataAnalysis.java
+++ src/DataAnalysis.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
- * DataAnalysis class performs various analytic tests on data
+ * DataAnalysis class performs various analytic tests on outcomes data
  * read in from the patient dataset
  * @author cbusc
  *
@@ -56,13 +57,80 @@ public class DataAnalysis {
 		}
 		return deceasedPatients;
 	}
+	
+	/**
+	 * Maps age as decade to number released; data for population chart
+	 * @return decade, numb released
+	 */
+	public HashMap<String, Integer> getAgeAsDecadeToNumReleased()
+	{
+	}
+	/**
+	 * Maps age as decade to number deceased; data for population chart
+	 * @return decade, numb deceased
+	 */
+	public HashMap<String, Integer> getAgeAsDecadeToNumDeceased()
+	{
+	}
+	
+	/**
+	 * Maps date to number infected; data for time series line chart
+	 * @return date, number infected
+	 */
+	public HashMap<String, Integer> getDateToNumInfected()
+	{
+	}
+	/**
+	 * Maps date to no number released; data for time series line chart
+	 * @return date, number released
+	 */
+	public HashMap<String, Integer> getDateToNumReleased()
+	{
+	}
+	/**
+	 * Maps date to number deceased; data for line chart
+	 * @return date, number deceased
+	 */
+	public HashMap<String, Integer> getDateToNumDeceased()
+	{
+	}
+	
+	/**
+	 * Maps gender to number released; data for unspecified chart
+	 * @return gender, number released
+	 */
+	public HashMap<String, Integer> getGenderToNumberReleased()
+	{
+	}
+	/**
+	 * Maps gender to number deceased; data for unspecified chart
+	 * @return gender, number deceased
+	 */
+	public HashMap<String, Integer> getGenderToNumberDeceased()
+	{
+	}
+	
+	/**
+	 * Maps healthcare-exposure to number released; data for unspecified chart
+	 * @return gender, number released
+	 */
+	public HashMap<Boolean, Integer> getHealthcareExposureToNumberReleased()
+	{
+	}
+	/**
+	 * Maps healthcare-exposure to number deceased; data for unspecified chart
+	 * @return gender, number deceased
+	 */
+	public HashMap<Boolean, Integer> getHealthcareExposureToNumberDeceased()
+	{
+	}
 
 	/**
-	 * Utility method to calculate mean age
+	 * Utility method to calculate average age
 	 * @param pArray
-	 * @return
+	 * @return average age
 	 */
-	public static double getAverageAge(ArrayList<Patient> pArray) 
+	public static double getAveAge(ArrayList<Patient> pArray) 
 	{
 		double sum = 0;
 		int count = 0;         
