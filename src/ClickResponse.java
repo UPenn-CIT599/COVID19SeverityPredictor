9ee3b42418d870862df7fb83131a1814abb3fd68
Lzimng <50159891+Lzimng@users.noreply.github.com>
2020-04-05 22:29:20 -0400

    Add files via upload
    GUI panel

---
+++ src/ClickResponse.java
@@ -0,0 +1,15 @@
+
+public class ClickResponse {
+	public ClickResponse() {
+		
+	}
+	
+	
+	/**
+	 * when user clicked the "calculation" button, this method will call UserInformationCollector
+	 * first, pass the array into classifier. Then call GraphGenerator to display the final result.
+	 */
+	public void response() {
+		
+	}
+}
